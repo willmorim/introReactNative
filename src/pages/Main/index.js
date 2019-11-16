20ddfb18168403e16b346020a654be16ce3504c8
willmorim <pdbawilliam@gmail.com>
2019-11-16 18:42:59 -0300

    Estilizando formulario

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <Container />
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar Usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
 
